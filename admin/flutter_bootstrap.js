20f6229ec545618648b665b14eab2746d998a66d
Ali Abdulkarim Elhassan <Alikarim4r@gmail.com>
2026-08-14 02:57:37 +0300

    deploy: complete 1.3.3 controlled fix removal

--- admin/flutter_bootstrap.js
+++ admin/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4147236586" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4279005476" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
